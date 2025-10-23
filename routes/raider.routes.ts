cc979d2d365c609b3bca9ca9a478a0744c3aaa82
possawat2185 <65011212185@msu.ac.th>
2025-10-24 04:22:57 +0700

    route raider

--- routes/raider.routes.ts
+++ routes/raider.routes.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { createRaider, deleteRaider, getRaiderById, getRaiders, loginRaider, updateRaider } from "../controllers/raiders";
 
 
 export const router = express.Router();
 
-router
+router.route('/').get(getRaiders).post(createRaider);
+router.route('/:id').get(getRaiderById).put(updateRaider).delete(deleteRaider);
+router.route('/login').get(loginRaider);
